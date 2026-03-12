5e01e61cd1639141fa64cc90e043d4935d3bd712
Gabriel Lopez <gabriel.lopez@epitech.eu>
2026-03-12 16:37:43 +0100

    feat: improve counter CSS styles

--- sample-app-master/resources/views/welcome.blade.php
+++ sample-app-master/resources/views/welcome.blade.php
@@ -1,20 +1,31 @@
-<html>
+<!DOCTYPE html>
+<html lang="fr">
     <head>
-    <script
-        src="https://code.jquery.com/jquery-3.7.0.min.js"
-        integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g="
-        crossorigin="anonymous"></script>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Counter App</title>
+        <link rel="stylesheet" href="/css/app.css">
+        <script
+            src="https://code.jquery.com/jquery-3.7.0.min.js"
+            integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g="
+            crossorigin="anonymous"></script>
     </head>
     <body>
-        <h1>Hello world sample app</h1>
-        <p>Counter :<p id="value">{{ $value }}</p></p>
-        <button id="add">+1</button>
+        <div class="container">
+            <h1>Sample App</h1>
+            <p class="counter-label">Counter</p>
+            <p id="value">{{ $value }}</p>
+            <button id="add">+1</button>
+        </div>
 
         <script>
             $(document).ready(function(){
                 $("#add").click(function(e){
                     $.get("/api/counter/add", function(data){
-                        $('#value').text(data.value);
+                        var $val = $('#value');
+                        $val.text(data.value);
+                        $val.addClass('bump');
+                        setTimeout(function(){ $val.removeClass('bump'); }, 200);
                     });
                 });
             });
